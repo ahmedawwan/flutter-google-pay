ea0a76b212afbcf1c16236d40d49a5f76456355f
unknown <ahmedsaeed0973@gmail.com>
2022-10-06 22:50:52 +0500

    issues fixed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -13,6 +13,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  // https://levelup.gitconnected.com/google-pay-in-flutter-1d9a848cc11a
   final paymentItems = [
     const pay.PaymentItem(
       label: 'Total',
@@ -37,18 +39,18 @@ class _HomeScreenState extends State<HomeScreen> {
     // Send the resulting Google Pay token to your server or PSP
     log(paymentResult.toString());
     String? clientSecret = await StripeController().paymentIntent({
-      'amount': '500',
+      'amount': '50',
       'currency': 'USD',
       'payment_method_types[]': 'card',
     });
     log('Client Secret: $clientSecret');
     final token =
         paymentResult['paymentMethodData']['tokenizationData']['token'];
-   final tokenJson = Map.castFrom(json.decode(token));
-   log(tokenJson.toString());
+    final tokenJson = Map.castFrom(json.decode(token));
+    log(tokenJson.toString());
 
     final params = PaymentMethodParams.cardFromToken(
-      paymentMethodData: tokenJson['id'],
+      paymentMethodData: tokenJson['messageId'],
     );
     await Stripe.instance.confirmPayment(
       clientSecret!,
